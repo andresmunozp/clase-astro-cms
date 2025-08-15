608a89795e34b9b5b0d7f39eab27f013c7ec2787
andresmunozp <amunozprado@unicauca.edu.co>
2025-08-14 22:33:28 -0500

    ssr mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig, envField } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    output: 'server',
     env: {
         schema:{
             STRAPI_URL: envField.string({context: 'server', access:'public'}),
