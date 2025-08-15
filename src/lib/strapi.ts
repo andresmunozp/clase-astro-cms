c9c436c5a6da5efa8d5125641a4dd3d618ba6770
andresmunozp <amunozprado@unicauca.edu.co>
2025-08-14 20:37:42 -0500

    inicio conectividad strapi

--- src/lib/strapi.ts
+++ src/lib/strapi.ts
@@ -1,2 +1,53 @@
-// Este archivo está preparado para la futura integración con Strapi.
-// Por ahora, la lógica de datos estáticos se encuentra en src/lib/data/
+import { STRAPI_URL , getSecret } from "astro:env/server";
+const STRAPI_TOKEN = getSecret('STRAPI_TOKEN')
+
+const apiURL = `${STRAPI_URL}/api`;
+
+interface Props {
+  endpoint: string;
+  query?: Record<string, string>;
+  wrappedByKey?: string;
+  wrappedByList?: boolean;
+}
+
+export default async function fetchApi<T>({
+  endpoint,
+  query,
+  wrappedByKey,
+  wrappedByList,
+  }: Props): Promise<T> {
+
+    if (endpoint.startsWith('/')) {
+        endpoint = endpoint.slice(1);
+    }
+
+    const url = new URL(`${apiURL}/${endpoint}`);
+
+    if (query) {
+        Object.entries(query).forEach(([key, value]) => {
+        url.searchParams.append(key, value);
+        });
+    }
+
+    const res = await fetch(url.toString(), {
+        headers: {
+        'Authorization': `bearer ${STRAPI_TOKEN}`
+        },
+    });
+
+    if(!res.ok) {
+        throw new Error(`Error fetching data from Strapi: ${res.status} ${res.statusText}`);
+    }
+
+    let data = await res.json();
+
+    if (wrappedByKey) {
+        data = data[wrappedByKey];
+    }
+
+    if (wrappedByList) {
+        data = data[0];
+    }
+
+    return data as T;
+}
